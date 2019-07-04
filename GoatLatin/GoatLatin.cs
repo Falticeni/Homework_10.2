af705d498c3f015822aa65c457ef86ffe4497515
Ciocan Robert Vasile <ciocanrobertvasile91@gmail.com>
2019-07-04 19:20:29 +0300

    Goat Latin resolved

--- GoatLatin/GoatLatin.cs
+++ GoatLatin/GoatLatin.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace GoatLatin
 {
-    public class GoatLatin
+    public static class GoatLatin
     {
+        public static string ToGoatLatin(string S)
+        {
+            string[] sentence = S.Split(' ');
+
+            StringBuilder sb = new StringBuilder();
+            for (int word = 0; word < sentence.Length; word++)
+            {
+                if ((sentence[word].ToLower()[0] == 'a') ||
+                    (sentence[word].ToLower()[0] == 'e') ||
+                    (sentence[word].ToLower()[0] == 'i') ||
+                    (sentence[word].ToLower()[0] == 'o') ||
+                    (sentence[word].ToLower()[0] == 'u'))
+                {
+                    sb.Append(sentence[word]);
+                    sb.Append("ma");
+                }
+                else
+                {
+                    sb.Append(sentence[word].Substring(1)); // intoarce un subsir existent incepand cu un anumit index
+                    sb.Append(sentence[word][0]);
+                    sb.Append("ma");
+                }
+                for (int i = 0; i <= word; i++)
+                {
+                    sb.Append('a');
+                }
+                if (word < sentence.Length - 1)
+                {
+                    sb.Append(" ");
+                }
+            }
+
+            return sb.ToString();
+
+
+        }
     }
 }
